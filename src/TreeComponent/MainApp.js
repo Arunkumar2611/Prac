7908d6a6a2cae21f5b79f0e27ae243476abeaafc
Arun <arunkumar923gole@gmail.com>
2023-05-15 13:29:23 +0530

    format and remove unused code

--- src/TreeComponent/MainApp.js
+++ src/TreeComponent/MainApp.js
@@ -2,103 +2,72 @@ import React from 'react'
 import TreeDynamicData from './TreeDynamicData';
 import Form from './Form';
 import { findNodeOriginal } from './Helpers/findNodeOriginal';
+import { yellow } from '@mui/material/colors';
 
 const MainApp = () => {
-    const [newData, setNewData] = React.useState([
-      {
-        "name": "arun",
-        "children": [],
-        "parent": "rakesh",
-        "level": 1
+  const [newData, setNewData] = React.useState([
+    {
+      "logical": "AND",
+      "name": 0,
+      "children": [],
+      "parent": 0,
     }
-  ]); 
-    const [newDataaa, setNewDataaa] = React.useState([
-      {
-        "name": "arun",
-        "children": [],
-        "parent": "rakesh",
-        "level": 1
+  ]);
+
+
+  const handleClick = (event, incoming) => {
+    console.log("%cInside handler name: ", "color: white; background:gray", incoming.name);
+    console.log("%cInside handler parent: ", "color: white; background:gray", incoming.parent);
+    console.log("%cInside handler logical: ", "color: white; background:gray", incoming.logical);
+
+
+    // console.log("arun", incoming)
+    let { parent } = incoming;
+    // console.log("incoming", incoming);
+
+    if (newData.length === 0) {
+      setNewData((oldData) => [...oldData, incoming]);
+      // console.log("%cFIRST COMPONENT", "color: orange", incoming);
+    } else {
+      console.log("adding a child");
+      console.log("new", newData)
+      let currentData = newData[0];
+      if (!parent) {
+        parent = currentData.name;
+        incoming.parent = parent;
+      }
+      // console.log("currentData", currentData, "parent", currentData.name);
+      // we need to loop throught the current tree to find the matching parent
+      // once found, we need to push the new node to the array of children of that parent node
+      const newChild = findNodeOriginal(parent, incoming, currentData);
+      setNewData([newChild]);
+
     }
-    ]);
+  }
 
-    function handleSubmitOriginal(event, incoming) {
-        event.preventDefault();
-        let { parent } = incoming;
-        console.log("incoming", incoming);
-    
-        if (newData.length === 0) {
-          setNewData((oldData) => [...oldData, incoming]);
-          setNewDataaa((oldData) => [...oldData, incoming]);
-          console.log("%cFIRST COMPONENT", "color: orange", incoming);
-        } else {
-          console.log("adding a child");
-          let currentData = newData[0];
-          if (!parent) {
-            parent = currentData.name;
-            incoming.parent = parent;
-          }
-          console.log("currentData", currentData, "parent", currentData.name);
-          // we need to loop throught the current tree to find the matching parent
-          // once found, we need to push the new node to the array of children of that parent node
-          const newChild = findNodeOriginal(parent, incoming, currentData);
-          setNewData([newChild]);
-        }
-      }
+  const removeFunc = (name, arr) => {
+    return arr
+      .filter((el) => el.name !== name)
+      .map((el) => {
+        if (!el.children || !Array.isArray(el.children)) return el;
+        el.children = removeFunc(name, el.children);
+        return el;
+      });
+  }
 
-    
-      const handleClick = (event, incoming) => {
-        console.log("arun", incoming)
-        let { parent } = incoming;
-        console.log("incoming", incoming);
-    
-        if (newData.length === 0) {
-          setNewData((oldData) => [...oldData, incoming]);
-          setNewDataaa((oldData) => [...oldData, incoming]);
-          console.log("%cFIRST COMPONENT", "color: orange", incoming);
-        } else {
-          console.log("adding a child");
-          console.log("new", newDataaa)
-          let currentData = newDataaa[0];
-          if (!parent) {
-            parent = currentData.name;
-            incoming.parent = parent;
-          }
-          console.log("currentData", currentData, "parent", currentData.name);
-          // we need to loop throught the current tree to find the matching parent
-          // once found, we need to push the new node to the array of children of that parent node
-          const newChild = findNodeOriginal(parent, incoming, currentData);
-          setNewDataaa([newChild]);
-          setNewData([newChild]);
-          
-        }
-      }
+  const recursiveRemove = (name) => {
+    const result = removeFunc(name, newData);
+    setNewData(result)
+  }
 
-      const removeFunc = (name, arr) => {
-        console.log("*****************name",name, "****************Arr", arr)
-        return  arr
-          .filter((el) => el.name !== name)
-          .map((el) => {
-            if (!el.children || !Array.isArray(el.children)) return el;
-            el.children = removeFunc(name, el.children);
-            return el;
-          });
-      }
+  console.log("newData", newData);
+  return (
+    <div>
+      <h1>Tree</h1>
+      <TreeDynamicData dynamicData={newData} handleClick={handleClick} recursiveRemove={recursiveRemove} />
 
-      const recursiveRemove = (name) => {
-        const result = removeFunc(name, newDataaa);
-        setNewDataaa(result)
-      } 
-
-console.log("newData", newData);
-console.log("newDataaaa", newDataaa);
-    return (
-        <div>
-            <h1>Tree</h1>
-            <Form handleSubmit={handleSubmitOriginal} />        
-            <TreeDynamicData dynamicData={newData} handleClick={handleClick} recursiveRemove={recursiveRemove} />
-
-        </div>
-    )
+    </div>
+  )
 }
 
 export default MainApp
--- src/TreeComponent/TreeDynamicData.js
+++ src/TreeComponent/TreeDynamicData.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ListItem from "./ListItem";
+import { Box } from "@mui/material";
 
 export default function TreeDynamicData(props) {
   const { dynamicData, level = 0, handleClick, recursiveRemove } = props;
-  console.log("%cinside tree", "color: red", dynamicData, "level", level);
+  // console.log("%cinside tree", "color: red", dynamicData, "level", level);
   if (!dynamicData || !dynamicData.length) return null;
 
   return (
-    <div style={{
+    <Box style={{
       backgroundColor: 'gray',
       border: '1px solid',
       padding: '10px',
@@ -15,11 +16,10 @@ export default function TreeDynamicData(props) {
     }}>
       {dynamicData.map((item) => (
         <div id={item.name} className="item" key={item.name}>
-          
           <ListItem item={item} level={level} handleClick={handleClick} recursiveRemove={recursiveRemove} />
           <TreeDynamicData dynamicData={item.children} level={level + 1} handleClick={handleClick} recursiveRemove={recursiveRemove} />
         </div>
       ))}
-    </div>
+    </Box>
   );
 }
